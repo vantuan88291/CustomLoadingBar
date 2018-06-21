c3725642c67e671d6fd5b73dcaac022593e745eb
Le van Tuan <vantuan88291@gmail.com>
2018-06-21 10:23:13 +0700

    add new set color of progessbar

--- app/src/main/java/com/tuan88291/customloadingbar/CustomLoadingBar.java
+++ app/src/main/java/com/tuan88291/customloadingbar/CustomLoadingBar.java
@@ -2,18 +2,22 @@ package com.tuan88291.customloadingbar;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class CustomLoadingBar extends FrameLayout{
 
     private TextView mLoadtext;
+    private ProgressBar mLoading;
 
     public CustomLoadingBar(@NonNull Context context) {
         super(context);
@@ -41,10 +45,16 @@ public class CustomLoadingBar extends FrameLayout{
     private void setUpView(Context context, AttributeSet attrs){
         LayoutInflater.from(context).inflate(R.layout.custom_loading, this, true);
         this.mLoadtext = findViewById(R.id.textload);
+        this.mLoading = findViewById(R.id.loading);
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CustomLoadingbar);
         String mtext = a.getString(R.styleable.CustomLoadingbar_loadingText);
         this.mLoadtext.setTextColor(a.getColor(R.styleable.CustomLoadingbar_loadingTextColor, ContextCompat.getColor(context, R.color.colorAccent)));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ColorStateList colorLoad = ColorStateList.valueOf(a.getColor(R.styleable.CustomLoadingbar_loadingBarColor, ContextCompat.getColor(context, R.color.colorAccent)));
+            this.mLoading.setIndeterminateTintList(colorLoad);
+        }
 
         setTextLoading(mtext);
 
